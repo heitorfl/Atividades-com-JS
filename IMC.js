a6587d8f5ce75fd7ab30e369436202b5137e5b16
Heitor Lima <141742426+heitorfl@users.noreply.github.com>
2024-09-18 08:02:06 -0300

    Update IMC.js

--- IMC.js
+++ IMC.js
@@ -1,6 +1,24 @@
 const peso = parseFloat(prompt("Digite seu peso"))
 const altura = parseFloat(prompt("Digite sua altura(em cm)"))
 
-const imc = peso/altura 
+const imc = peso/(altura*altura) 
 
-alert(imc)
+if(imc<18.5){
+  alert("Magreza")
+}
+
+else if(18.5<=imc<25){
+  alert("Normal")
+}
+
+else if(25<=imc<30){
+  alert("Sobrepeso")
+}
+
+else if(30<=imc<40){
+  alert("Obesidade")
+}
+
+else{
+  alert("Obesidade grave")
+}
