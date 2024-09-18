3a041a82765ace6e153987d78e95dfb0a3ee90a8
Heitor Lima <141742426+heitorfl@users.noreply.github.com>
2024-09-18 07:34:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const nota1 = parseFloat(prompt("Digite a primeira nota: "))
+const nota2 = parseFloat(prompt("Digite a segunda nota: "))
+const nota3 = parseFloat(prompt("Digite a terceira nota: "))
+
+const res = (nota1+nota2+nota3)/3
+
+alert(res)
